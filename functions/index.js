6bf5bc4e671627d63651b20fa1971c932e3053df
Xrhoys <yefeng@hotmail.fr>
2020-10-15 23:33:30 +0200

    init

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const express = require('express')
+const { ref } = require('firebase-functions/lib/providers/database')
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const app = express()
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://bug-tracker-acb50.firebaseio.com/',
+})
+
+admin.database().ref('test').child('test').set({ test: 'test' })
+
+app.get('/hello', (req, res) => res.send('hell o world'))
+
+exports.app = functions.https.onRequest(app)
